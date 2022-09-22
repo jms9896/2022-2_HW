dc297505247478702684b6ac074790c50d7e17d6
jms9896 <jms9896@gmail.com>
2022-09-22 14:49:01 +0900

    HW1-1 완성!

--- datamining_HW/HW1/HW1-1.py
+++ datamining_HW/HW1/HW1-1.py
@@ -22,16 +22,29 @@ for i in range(1, len(high_temp_8)+1): # 1부터 31까지니까 31개
     x_val_day.append(i + 1)
     if '' != high_temp_8[i-1]: # 실수로 변경
         high_temp_8[i-1] = float(high_temp_8[i-1])
+    elif i==1:
+        high_temp_8[i - 1] = float(high_temp_8[i])
+    elif i==len((high_temp_8)+1):
+        high_temp_8[i - 1] = float(high_temp_8[i-2])
+    else:
+        high_temp_8[i - 1] = (float(high_temp_8[i])+float(high_temp_8[i-2]))/2
+
 
     if '' != low_temp_8[i-1]:
         low_temp_8[i-1] = float(low_temp_8[i-1])
+    elif i == 1:
+        low_temp_8[i - 1] = float(low_temp_8[i])
+    elif i == (len(high_temp_8) + 1):
+        low_temp_8[i - 1] = float(low_temp_8[i - 2])
+    else:
+        low_temp_8[i - 1] = (float(low_temp_8[i]) + float(low_temp_8[i - 2])) / 2
 
 print(high_temp_8)
 print(low_temp_8)
 
 
-plt.plot(x_val_day, high_temp_8, color='b', label="8월고온")
-plt.plot(x_val_day, low_temp_8, color='r', label="8월저온")
+plt.plot(x_val_day, high_temp_8, color='r', label="8월고온")
+plt.plot(x_val_day, low_temp_8, color='b', label="8월저온")
 plt.xlabel('day')
 plt.ylabel('temperature')
 #plt.ylim(5, 50)
